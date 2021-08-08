31471a460badb5511d028d1100a3f6340be7ebee
Ilya Bozhkov <ilia.il.27@mail.ru>
2021-08-08 11:12:26 +0300

    Added translation to the postfix record and tests

--- src/main/kotlin/Calculator.kt
+++ src/main/kotlin/Calculator.kt
@@ -8,58 +8,58 @@ class Calculator {
         for (i in expression.indices) {
             if (expression[i] in '0'..'9' || expression[i] == '.') {
                 postfixExpression.add(expression[i])
-            } else {
-                if (expression[i] != ' ') {
-                    if (stack.isNotEmpty()) {
-                        when (expression[i]) {
-                            in highPriority -> {
-                                when (stack.last()) {
-                                    in lowPriority -> stack.add(expression[i])
-                                    in highPriority -> {
-                                        postfixExpression.add(stack.last())
-                                        stack.remove(stack.last())
-                                        stack.add(expression[i])
-                                    }
-                                    else -> stack.add(expression[i])
+                continue
+            }
+            if (expression[i] != ' ') {
+                if (stack.isNotEmpty()) {
+                    when (expression[i]) {
+                        in highPriority -> {
+                            when (stack.last()) {
+                                in lowPriority -> stack.add(expression[i])
+                                in highPriority -> {
+                                    postfixExpression.add(stack.last())
+                                    stack.remove(stack.last())
+                                    stack.add(expression[i])
                                 }
+                                else -> stack.add(expression[i])
                             }
-                            in lowPriority -> {
-                                when (stack.last()) {
-                                    in highPriority -> {
-                                        postfixExpression.add(stack.last())
-                                        stack.remove(stack.last())
-                                        stack.add(expression[i])
-                                    }
-                                    in lowPriority -> {
-                                        postfixExpression.add(stack.last())
-                                        stack.remove(stack.last())
-                                        stack.add(expression[i])
-                                    }
-                                    else -> stack.add(expression[i])
+                        }
+                        in lowPriority -> {
+                            when (stack.last()) {
+                                in highPriority -> {
+                                    postfixExpression.add(stack.last())
+                                    stack.remove(stack.last())
+                                    stack.add(expression[i])
                                 }
+                                in lowPriority -> {
+                                    postfixExpression.add(stack.last())
+                                    stack.remove(stack.last())
+                                    stack.add(expression[i])
+                                }
+                                else -> stack.add(expression[i])
                             }
-                            in bracket -> {
-                                if (expression[i] == '(') stack.add(expression[i])
-                                else {
-                                    if ('(' in stack) {
-                                        while (stack.last() != '(') {
-                                            postfixExpression.add(stack.last())
-                                            stack.removeAt(stack.lastIndexOf(stack.last()))
-                                        }
+                        }
+                        in bracket -> {
+                            if (expression[i] == '(') stack.add(expression[i])
+                            else {
+                                if ('(' in stack) {
+                                    while (stack.last() != '(') {
+                                        postfixExpression.add(stack.last())
                                         stack.removeAt(stack.lastIndexOf(stack.last()))
                                     }
+                                    stack.removeAt(stack.lastIndexOf(stack.last()))
                                 }
                             }
                         }
-                    } else stack.add(expression[i])
-                } else {
-                    if (expression[i + 1] in '0'..'9' && expression[i] != '.') postfixExpression.add(' ')
-                }
+                    }
+                } else stack.add(expression[i])
+            } else {
+                if (expression[i + 1] in '0'..'9' && expression[i] != '.') postfixExpression.add(' ')
             }
         }
         while (stack.isNotEmpty()) {
             postfixExpression.add(stack.last())
-            stack.remove(stack.last())
+            stack.removeAt(stack.lastIndexOf(stack.last()))
         }
         return postfixExpression
     }
