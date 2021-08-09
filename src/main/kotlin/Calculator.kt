65c8d8066ce88184935c568350e8341b9eda6a1f
Bob <gucciglasses@gmail.com>
2021-08-09 16:28:40 +0300

    Added methods

--- src/main/kotlin/Calculator.kt
+++ src/main/kotlin/Calculator.kt
@@ -1,3 +1,17 @@
-class Calculator(val A: Float, val B: Float) {
-    fun Plus(): Float{return A + B}
+class Calculator {
+    fun plus(leftOperand: Double, rightOperand:Double) =  leftOperand + rightOperand
+    fun minus(leftOperand: Double, rightOperand: Double) = leftOperand - rightOperand
+    fun multiply(leftOperand: Double, rightOperand: Double) = leftOperand * rightOperand
+    fun divide(leftOperand: Double, rightOperand: Double): Double?{
+        try {
+            if (rightOperand == 0.0)
+                throw Exception("Division by zero!")
+            else
+                return leftOperand/rightOperand
+        }
+        catch (e: Exception){
+            print(e.message)
+            return null
+        }
+    }
 }
