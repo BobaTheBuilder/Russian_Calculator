2c7068f6ec318e3fce269ebf514eebf8a74d5ade
Bob <gucciglasses@gmail.com>
2021-08-10 03:28:59 +0300

    Added postfix notation to answer method + test on that method

--- src/main/kotlin/Calculator.kt
+++ src/main/kotlin/Calculator.kt
@@ -5,7 +5,7 @@ class Calculator {
     fun divide(leftOperand: Double, rightOperand: Double): Double?{
         try {
             if (rightOperand == 0.0)
-                throw Exception("Division by zero!")
+                throw Exception("Division by zero!\n")
             else
                 return leftOperand/rightOperand
         }
@@ -14,7 +14,48 @@ class Calculator {
             return null
         }
     }
-    fun postfixNotationToAnswer(postfixNotation: String): Double{
-        
+    fun postfixNotationToAnswer(postfixNotation: String): Double?{
+        var answer: MutableList<Double?> = mutableListOf()
+        var temp: String = ""
+        fun addNumberToArray(){
+            if (temp!="")
+                answer.add(temp.toDoubleOrNull())
+            temp = ""
+        }
+        for (i in postfixNotation.indices){
+            when (postfixNotation[i]){
+                ' ' -> {
+                    addNumberToArray()
+                }
+                in '0'..'9', '.'-> {
+                    temp += postfixNotation[i]
+                }
+                '+' -> {
+                    addNumberToArray()
+                    var lastIndex = answer.lastIndexOf(answer.last())
+                    answer[lastIndex - 1] = plus(answer[lastIndex - 1] ?: 0.0,answer.last() ?: 0.0)
+                    answer.removeAt(lastIndex)
+                }
+                '-' -> {
+                    addNumberToArray()
+                    var lastIndex = answer.lastIndex
+                    answer[lastIndex - 1] = minus(answer[lastIndex - 1] ?: 0.0,answer.last() ?: 0.0)
+                    answer.removeAt(lastIndex)
+                }
+                '*' -> {
+                    addNumberToArray()
+                    var lastIndex = answer.lastIndexOf(answer.last())
+                    answer[lastIndex - 1] = multiply(answer[lastIndex - 1] ?: 0.0,answer.last() ?: 0.0)
+                    answer.removeAt(lastIndex)
+                }
+                '/' -> {
+                    addNumberToArray()
+                    var lastIndex = answer.lastIndexOf(answer.last())
+                    answer[lastIndex - 1] = divide(answer[lastIndex - 1] ?: 0.0,answer.last() ?: 0.0)
+                    answer.removeAt(lastIndex)
+                }
+            }
+        }
+        return answer[0]
     }
 }
--- src/test/kotlin/test tut.kt
+++ src/test/kotlin/test tut.kt
@@ -1,3 +1,23 @@
-class `test tut` {
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions.*
 
+class CalculatorTest {
+    @Test
+    fun `postfixToInt method test expression 1`(){
+        val expected = 8.0
+        val actual: Double? = Calculator().postfixNotationToAnswer("45 5- 68 29 2*-/ 2*")
+        assertEquals(expected,actual)
+    }
+    @Test
+    fun `postfixToInt method test expression 2`(){
+        val expected = -134.0
+        val actual: Double? = Calculator().postfixNotationToAnswer("2 2+ 2 2*+ 2+ 4 4* 2/ 2* 9*-")
+        assertEquals(expected,actual)
+    }
+    @Test
+    fun `postfixToInt method test "Division by 0"`(){
+        val expected = null
+        val actual: Double? = Calculator().postfixNotationToAnswer("55 10 2 5*-/")
+        assertEquals(expected,actual)
+    }
 }
