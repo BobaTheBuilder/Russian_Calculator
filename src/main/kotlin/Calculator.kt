367a53b291c152526364f78bfb7666dcd767f649
Ilya Bozhkov <ilia.il.27@mail.ru>
2021-08-12 17:54:16 +0300

    Added a method for checking expressions and tests

--- src/main/kotlin/Calculator.kt
+++ src/main/kotlin/Calculator.kt
@@ -1,3 +1,16 @@
-class Calculator(val A: Float, val B: Float) {
-    fun Plus(): Float{return A + B}
+class Calculator() {
+    fun checkingExpression(expression: String): Boolean {
+        val operations = listOf<Char>('+', '-', '*', '/')
+        val invalidCharacters = listOf<Char>(',', '[', ']', '{', '}')
+        val bracket = listOf('(', ')')
+        var tmp = false
+        for (i in expression.indices) {
+            if (expression[i] in operations && expression[i + 1] in operations) return false
+            if (expression[i] in 'a'..'z' || expression[i] in 'A'..'Z') return false
+            if (expression[i] in invalidCharacters) return false
+            if (expression[i] == '(' && expression[i + 1] in operations) return false
+            if (expression[i] == ')' && expression[i - 1] in operations) return false
+        }
+        return expression.count { simbol -> simbol == '(' } == expression.count { simbol -> simbol == ')' }
+    }
 }
